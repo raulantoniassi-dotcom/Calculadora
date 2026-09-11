e296879c1b06df754e741761785d47b289023060
RAUL FRANCO ANTONIASSI <raul_antoniassi@estudante.sesisenai.org.br>
2026-09-10 22:03:39 -0300

    Projeto Final Formatado

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
@@ -7,51 +7,41 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
   res.send("Servidor da Calculadora funcionando!");
 });
 
 // Rota do Cálculo
-app.post('/calculo', (req, res) => {
+app.post("/calculo", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  const operacao = req.body.operacao;
 
-    const num1 = Number(req.body.num1);
-    const num2 = Number(req.body.num2);
-    const operacao = req.body.operacao;
-    
-    let resultado;
+  let resultado;
 
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.status(400).json({ erro: "Números inválidos fornecidos." });
+  }
 
-    if (isNaN(num1) || isNaN(num2)) {
-        return res.status(400).json({ erro: "Números inválidos fornecidos." });
+  if (operacao === "+") {
+    resultado = num1 + num2;
+  } else if (operacao === "-") {
+    resultado = num1 - num2;
+  } else if (operacao === "*") {
+    resultado = num1 * num2;
+  } else if (operacao === "/") {
+    if (num2 === 0) {
+      return res.json({ resultado: "Erro: Divisão por 0" });
     }
+    resultado = num1 / num2;
+  } else if (operacao === "%") {
+    resultado = num1 % num2;
+  } else {
+    return res.status(400).json({ erro: "Operação inválida." });
+  }
 
-    if (operacao === "+"){
-      resultado = num1 + num2;
-    }
-    else if(operacao === "-"){
-      resultado = num1 - num2;
-    }
-    else if(operacao === "*"){
-      resultado = num1 * num2;
-    }
-    else if(operacao === "/"){
-      if (num2 === 0) {
-          return res.json({ resultado: "Erro: Divisão por 0" });
-      }
-      resultado = num1 / num2;
-    }
-    else if(operacao === "%"){
-      resultado = num1 % num2;
-    }
-    else {
-      return res.status(400).json({ erro: "Operação inválida." });
-    }
-
-
-    res.json({ resultado: resultado });
+  res.json({ resultado: resultado });
 });
-
 
 app.listen(port, () => {
   console.log(`Calculadora API rodando com sucesso na porta ${port}`);
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,26 @@
-
 // Base
 let a = [];
 let b = [];
-let operador = false; 
+let operador = false;
 let operacaoAtual = "";
 
 // Display
 let display = document.querySelector("#display");
 
-function addNumber(number){
-    display.value += number;
+function addNumber(number) {
+  display.value += number;
 }
 
 // Botões pra apagar
 limpar.addEventListener("click", () => {
-    display.value = "";
-    a = [];
-    b = [];
-    operador = false;
-    operacaoAtual = "";
+  display.value = "";
+  a = [];
+  b = [];
+  operador = false;
+  operacaoAtual = "";
 });
 
-const backspace = document.getElementById("Backspace")
-
+const backspace = document.getElementById("Backspace");
 
 // Números
 const num1 = document.getElementById("1");
@@ -37,152 +35,158 @@ const num9 = document.getElementById("9");
 const num0 = document.getElementById("0");
 
 num1.addEventListener("click", () => {
-    if (operador == false){
-        a.push(1);
-    } else if (operador == true){
-        b.push(1);
-    }
-    addNumber(1);
-})
+  if (operador == false) {
+    a.push(1);
+  } else if (operador == true) {
+    b.push(1);
+  }
+  addNumber(1);
+});
 
 num2.addEventListener("click", () => {
-    if (operador == false){
-        a.push(2);
-    } else if (operador == true){
-        b.push(2);
-    }
-    addNumber(2);
-})
+  if (operador == false) {
+    a.push(2);
+  } else if (operador == true) {
+    b.push(2);
+  }
+  addNumber(2);
+});
 
 num3.addEventListener("click", () => {
-    if (operador == false){
-        a.push(3);
-    } else if (operador == true){
-        b.push(3);
-    }
-    addNumber(3);
-})
+  if (operador == false) {
+    a.push(3);
+  } else if (operador == true) {
+    b.push(3);
+  }
+  addNumber(3);
+});
 
 num4.addEventListener("click", () => {
-    if (operador == false){
-        a.push(4);
-    } else if (operador == true){
-        b.push(4);
-    }
-    addNumber(4);
-})
+  if (operador == false) {
+    a.push(4);
+  } else if (operador == true) {
+    b.push(4);
+  }
+  addNumber(4);
+});
 
 num5.addEventListener("click", () => {
-    if (operador == false){
-        a.push(5);
-    } else if (operador == true){
-        b.push(5);
-    }
-    addNumber(5);
-})
+  if (operador == false) {
+    a.push(5);
+  } else if (operador == true) {
+    b.push(5);
+  }
+  addNumber(5);
+});
 
 num6.addEventListener("click", () => {
-    if (operador == false){
-        a.push(6);
-    } else if (operador == true){
-        b.push(6);
-    }
-    addNumber(6);
-})
+  if (operador == false) {
+    a.push(6);
+  } else if (operador == true) {
+    b.push(6);
+  }
+  addNumber(6);
+});
 
 num7.addEventListener("click", () => {
-    if (operador == false){
-        a.push(7);
-    } else if (operador == true){
-        b.push(7);
-    }
-    addNumber(7);
-})
+  if (operador == false) {
+    a.push(7);
+  } else if (operador == true) {
+    b.push(7);
+  }
+  addNumber(7);
+});
 
 num8.addEventListener("click", () => {
-    if (operador == false){
-        a.push(8);
-    } else if (operador == true){
-        b.push(8);
-    }
-    addNumber(8);
-})
+  if (operador == false) {
+    a.push(8);
+  } else if (operador == true) {
+    b.push(8);
+  }
+  addNumber(8);
+});
 
 num9.addEventListener("click", () => {
-    if (operador == false){
-        a.push(9);
-    } else if (operador == true){
-        b.push(9);
-    }
-    addNumber(9);
-})
+  if (operador == false) {
+    a.push(9);
+  } else if (operador == true) {
+    b.push(9);
+  }
+  addNumber(9);
+});
 
 num0.addEventListener("click", () => {
-    if (operador == false){
-        a.push(0);
-    } else if (operador == true){
-        b.push(0);
-    }
-    addNumber(0);
-})
+  if (operador == false) {
+    a.push(0);
+  } else if (operador == true) {
+    b.push(0);
+  }
+  addNumber(0);
+});
 
 // operadores
 const btnSoma = document.querySelector("#somar");
 btnSoma.addEventListener("click", () => {
-    operador = true;
-    operacaoAtual = "+";
-    addNumber(" + ");
+  operador = true;
+  operacaoAtual = "+";
+  addNumber(" + ");
 });
 
 const btnSubtrair = document.querySelector("#subtrair");
 btnSubtrair.addEventListener("click", () => {
-    operador = true;
-    operacaoAtual = "-";
-    addNumber(" - ");
+  operador = true;
+  operacaoAtual = "-";
+  addNumber(" - ");
 });
 
 const btnMultiplicar = document.querySelector("#multiplicar");
 btnMultiplicar.addEventListener("click", () => {
-    operador = true;
-    operacaoAtual = "*";
-    addNumber(" × ");
+  operador = true;
+  operacaoAtual = "*";
+  addNumber(" × ");
 });
 
 const btnDividir = document.querySelector("#dividir");
 btnDividir.addEventListener("click", () => {
-    operador = true;
-    operacaoAtual = "/";
-    addNumber(" ÷ ");
+  operador = true;
+  operacaoAtual = "/";
+  addNumber(" ÷ ");
+});
+
+const btnPorcento = document.querySelector("#porcento");
+btnPorcento.addEventListener("click", () => {
+  operador = true;
+  operacaoAtual = "%";
+  addNumber(" % ");
 });
 
 const btnIgual = document.querySelector("#igual");
 btnIgual.addEventListener("click", () => {
-    if (a.length === 0 || b.length === 0) return;
+  if (a.length === 0 || b.length === 0) return;
 
-    fetch("http://localhost:3000/calculo", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            num1: parseFloat(a.join("")),
-            num2: parseFloat(b.join("")),
-            operacao: operacaoAtual
-        })
+  fetch("http://localhost:3000/calculo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      num1: parseFloat(a.join("")),
+      num2: parseFloat(b.join("")),
+      operacao: operacaoAtual,
+    }),
+  })
+    .then((res) => res.json())
+    .then((resultado) => {
+      console.log(resultado);
+
+      display.value = resultado.resultado || resultado;
+
+      a = [display.value];
+      b = [];
+      operador = false;
     })
-    .then(res => res.json())
-    .then(resultado => {
-        console.log(resultado);
-      
-        display.value = resultado.resultado || resultado;
-        
-       
-        a = [display.value];
-        b = [];
-        operador = false;
-    })
-    .catch(err => {
-        console.error("Erro:", err);
-        display.value = "Erro";
+    .catch((err) => {
+      console.error("Erro:", err);
+      display.value = "Erro";
     });
 });
